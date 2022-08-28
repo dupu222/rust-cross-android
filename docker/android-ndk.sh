067720610dca0bfc5140ae017619134226be6ea6
dupu <dupu@shimo.im>
2022-08-28 23:03:03 +0800

    fix: ci build

--- docker/android-ndk.sh
+++ docker/android-ndk.sh
@@ -6,7 +6,7 @@ set -euo pipefail
 # shellcheck disable=SC1091
 . lib.sh
 
-NDK_URL=https://dl.google.com/android/repository/android-ndk-r15b-linux-x86_64.zip
+NDK_URL=https://dl.google.com/android/repository/android-ndk-r15c-linux-x86_64.zip
 
 main() {
     local arch="${1}" \
